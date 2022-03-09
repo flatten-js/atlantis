ea97991bad314f4abf45cb8e1144c66cbc6e836f
flatten-js <thetakahito@gmail.com>
2022-03-10 02:31:21 +0900

    Added explanations to each CLI operation

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ require('dotenv').config()
 
 const { program } = require('commander')
 
+const package = require('./package.json')
 const Atlantis = require('./lib/atlantis.js')
 const { sleep } = require('./lib/utils.js')
 
 const atlantis = new Atlantis(process.env.RSA_PUBLIC_KEY)
 
 program
+  .name('atlantis')
+  .description('Encrypted folders that appear only at runtime')
+  .version(package.version)
+
+program
   .command('create')
-  .argument('<src>')
+  .description('Create encrypted folder')
+  .argument('<src>', 'Path to the folder you want to encrypt')
   .action(src => atlantis.compress(src))
 
 program
-  .argument('<src>')
-  .argument('<key>')
+  .description('Expand encrypted folder')
+  .argument('<src>', 'Path to encrypted folder')
+  .argument('<key>', 'Path to private key')
   .action((src, key) => atlantis.uncompress(src, key))
 
 program.parse()
