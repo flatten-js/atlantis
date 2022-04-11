4a79bd7608a1ceef6dc2e9755748012c2d3d4d68
flatten-js <thetakahito@gmail.com>
2022-04-12 00:24:16 +0900

    Changed so that the specified source name is not displayed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { program } = require('commander')
 const package = require('./package.json')
 const Atlantis = require('./lib/atlantis.js')
 const { ALGORITHM } = require('./lib/crypt.js')
+const prompt = require('./lib/prompt.js')
 
 
 const atlantis = new Atlantis()
@@ -15,20 +16,20 @@ program
 program
   .command('create')
   .description('Create encrypted folder')
-  .argument('<src>', 'Path to the folder you want to encrypt')
   .option('-alg, --algorithm <algorithm>', 'Algorithm to encrypt', ALGORITHM.AES)
-  .action(async src => {
+  .action(async () => {
     const { algorithm } = program.opts()
+    const src = await prompt.secret('Please enter your folder to encrypt: ')
     const key = await Atlantis.key(algorithm, true)
     atlantis.compress(src, algorithm, key)
   })
 
 program
   .description('Expand encrypted folder')
-  .argument('<src>', 'Path to encrypted folder')
   .option('-alg, --algorithm <algorithm>', 'Algorithm to decrypt', ALGORITHM.AES)
-  .action(async src => {
+  .action(async () => {
     const { algorithm } = program.opts()
+    const src = await prompt.secret('Please enter your folder to decrypt: ')
     const key = await Atlantis.key(algorithm)
     atlantis.uncompress(src, algorithm, key)
   })
